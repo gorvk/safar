d863e2289c917f3179c4aa8e2789dec923776eae
Gourav kolhatkar <gstar1525@gmail.com>
2025-02-26 21:13:07 +0530

    added basic layout for Itinerary form

---
+++ src/components/Itinerary/CheckpointForm.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { ListUI } from "../../Icons/ListUl";
+import { Trash } from "../../Icons/Trash";
+import { TCheckpoint } from "../../types";
+
+export const CheckpointForm = (props: {
+  data: TCheckpoint;
+  deleteCheckpoint: () => void;
+}) => {
+  const [thingsToTry, setThingsToTry] = useState<string[]>(
+    props.data.things_to_try ?? []
+  );
+
+  const addThingsToTry = () => {
+    setThingsToTry([...thingsToTry, ""]);
+  };
+
+  return (
+    <div className="flex flex-col gap-4 bg-teal-600 p-5 rounded-lg sm:w-1/2">
+      <div className="flex w-full justify-between gap-6">
+        <input
+          defaultValue={props.data.title}
+          placeholder="Checkpoint title"
+          className="text-2xl outline-0 w-full"
+        />
+        <div className="cursor-pointer" onClick={addThingsToTry}>
+          <ListUI color="fill-white" />
+        </div>
+        <div className="cursor-pointer" onClick={props.deleteCheckpoint}>
+          <Trash />
+        </div>
+      </div>
+      <input placeholder="Time picker" className="text-2xl outline-0" />
+      {thingsToTry.map((value, index) => (
+        <input
+          key={index}
+          defaultValue={value}
+          placeholder="Things to try"
+          className="text-2xl outline-0"
+        />
+      ))}
+    </div>
+  );
+};
--- src/components/Itinerary/ItineraryForm.tsx
+++ src/components/Itinerary/ItineraryForm.tsx
@@ -1,1 +1,67 @@
-export const ItineraryForm = () => <h1>Itinerary Form</h1>;
+import { useState } from "react";
+import { Image } from "../../Icons/Image";
+import { ListUI } from "../../Icons/ListUl";
+import { CheckpointForm } from "./CheckpointForm";
+import { TCheckpoint, TItineraryDetail } from "../../types";
+
+export const ItineraryForm = (props: { data?: TItineraryDetail }) => {
+  const { data } = props;
+  const [checkpoints, setCheckpoints] = useState<TCheckpoint[]>(
+    data?.checkpoints ?? []
+  );
+
+  const addCheckpoint = () => {
+    setCheckpoints([...checkpoints, {} as TCheckpoint]);
+  };
+
+  const deleteCheckpoint = (index: number) => {
+    const a = [...checkpoints];
+    a.splice(index, 1);
+    setCheckpoints([...a]);
+  };
+
+  return (
+    <div className="m-4">
+      <div className="flex flex-col gap-4 mb-4">
+        <input
+          defaultValue={data?.title}
+          placeholder="Title"
+          className="text-5xl outline-0"
+        />
+        <input
+          defaultValue={data?.source}
+          placeholder="Source"
+          className="text-2xl outline-0"
+        />
+        <input
+          defaultValue={data?.destination}
+          placeholder="Destination"
+          className="text-2xl outline-0"
+        />
+        <input
+          defaultValue={data?.uploaded_duration}
+          placeholder="Journey date"
+          className="text-2xl outline-0"
+        />
+      </div>
+      <hr />
+      <div className="flex flex-col gap-6 my-4">
+        <div className="flex gap-6">
+          <div className="cursor-pointer">
+            <Image />
+          </div>
+          <div className="cursor-pointer" onClick={() => addCheckpoint()}>
+            <ListUI color="fill-teal-600" />
+          </div>
+        </div>
+        {data?.checkpoints.map((data, index) => (
+          <CheckpointForm
+            key={index}
+            data={data}
+            deleteCheckpoint={() => deleteCheckpoint(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
